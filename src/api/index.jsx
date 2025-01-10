2ad49c6836888b772f95e2bad2898052f284ca8e
Divyshekhar Sinha <divyshekharsinha@Divyshekhars-MacBook-Air.local>
2025-01-10 22:29:08 +0530

    changing the api url

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/posts/';
-// const url2= 'https://memories-backend-1ezd.onrender.com/posts';
+// const urlLocal = 'http://localhost:3000/posts/';
+const url= 'https://memories-backend-1ezd.onrender.com/posts';
 
 export const fetchPosts = async () => {
   try {
